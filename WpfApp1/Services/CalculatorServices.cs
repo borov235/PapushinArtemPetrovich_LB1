ebd888de2c15b27f5ca16e0794f2eb6a8cc37592
rtutb <rtutb@>
2026-09-14 23:37:56 +0300

    Добавьте файлы проекта.

---
+++ WpfApp1/Services/CalculatorServices.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleCalculator.Services
+{
+    public class CalculatorService
+    {
+        public double Calculate(double first, double second, string op)
+        {
+            switch (op)
+            {
+                case "+":
+                    return first + second;
+                case "-":
+                    return first - second;
+                case "*":
+                    return first * second;
+                case "/":
+                    return second != 0 ? first / second : 0;
+                default:
+                    return second;
+            }
+        }
+    }
+}
---
+++ WpfApp1/Views/MainWindow.xaml.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace SimpleCalculator.Views
+{
+    public partial class MainWindow : Window
+    {
+        private double _firstNumber;
+        private string _operation = "";
+        private bool _isNewEntry = true;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Number_Click(object sender, RoutedEventArgs e)
+        {
+            var btn = (Button)sender;
+            if (TxtDisplay.Text == "0" || _isNewEntry)
+            {
+                TxtDisplay.Text = btn.Content.ToString();
+                _isNewEntry = false;
+            }
+            else
+            {
+                TxtDisplay.Text += btn.Content.ToString();
+            }
+        }
+
+        private void Operator_Click(object sender, RoutedEventArgs e)
+        {
+            var btn = (Button)sender;
+            _firstNumber = double.Parse(TxtDisplay.Text);
+            _operation = btn.Content.ToString();
+            _isNewEntry = true;
+        }
+
+        private void Equals_Click(object sender, RoutedEventArgs e)
+        {
+            double secondNumber = double.Parse(TxtDisplay.Text);
+            double result = 0;
+
+            switch (_operation)
+            {
+                case "+": result = _firstNumber + secondNumber; break;
+                case "-": result = _firstNumber - secondNumber; break;
+                case "*": result = _firstNumber * secondNumber; break;
+                case "/": result = secondNumber != 0 ? _firstNumber / secondNumber : 0; break;
+            }
+
+            TxtDisplay.Text = result.ToString();
+            _isNewEntry = true;
+        }
+
+        private void Clear_Click(object sender, RoutedEventArgs e)
+        {
+            TxtDisplay.Text = "0";
+            _firstNumber = 0;
+            _operation = "";
+            _isNewEntry = true;
+        }
+    }
+}
